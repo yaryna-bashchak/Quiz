afebe9c797776af441cc3b6fc3fb0e91ae189d93
yaryna-bashchak <y.bashchak.im11@kpi.ua>
2022-11-16 13:25:07 +0200

    implement function to add option

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+const optionsList = document.getElementById('options-list');
+
+let optionCounter = 1;
+
 class Question {
-  constructor(question, choices, answer) {
+  constructor(question, options, answer) {
     this.question = question;
-    this.choices = choices;
+    this.options = options;
     this.answer = answer;
   }
 }
@@ -12,4 +16,12 @@ const question1 = new Question(
   6,
 );
 
-console.log({ ...question1 });
+console.log(question1);
+
+const addOption = () => {
+  const item = document.createElement('li');
+  optionCounter++;
+  item.innerHTML = `<input type="radio" name="answer"><input type="text" id="input-option${optionCounter}"></il><br>`;
+  optionsList.appendChild(item);
+  console.log('add choice');
+};
