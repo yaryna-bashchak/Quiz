776274f2534f9c4be09716c5904e4c9fdb0fd620
yaryna-bashchak <y.bashchak.im11@kpi.ua>
2022-11-26 20:03:55 +0200

    move class Question to separate file

---
+++ question.js
@@ -0,0 +1,27 @@
+const { elements } = require('./HTMLelements/1_create_quiz');
+
+module.exports = class {
+  constructor(question, options, answers) {
+    this.question = question;
+    this.options = options;
+    this.answers = answers;
+  }
+
+  printQuestion() {
+    elements.questionParagraph.textContent = `Task: ${this.question}`;
+    this.printOptions();
+  }
+
+  printOptions() {
+    for (let i = 0; i < this.options.length; i++) {
+      const item = document.createElement('li');
+      item.setAttribute('id', `item${i + 1}`);
+      item.innerHTML = `<input type="radio" name="answer" class="radio" value=${this.options[i]}><span>${this.options[i]}</span></il><br>`;
+      elements.optionsList.appendChild(item);
+    }
+  }
+
+  checkAnswer() {}
+
+  deleteOptions() {}
+};
