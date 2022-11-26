6ba1ce253288f4bfaf7b5e2a9a47dc6bcee10421
yaryna-bashchak <y.bashchak.im11@kpi.ua>
2022-11-26 20:04:25 +0200

    move getting HTML elements to separate file

---
+++ HTMLelements/1_create_quiz.js
@@ -0,0 +1,9 @@
+exports.elements = {
+  optionsList: document.getElementById('options-list'),
+  btnDeleteOption: document.getElementById('delete-option'),
+  btnAddOption: document.getElementById('add-option'),
+  btnReset: document.getElementById('reset-btn'),
+  btnAddQuestion: document.getElementById('add-question'),
+  btnStartQuiz: document.getElementById('start-quiz'),
+  questionParagraph: document.getElementById('question-text'),
+};
